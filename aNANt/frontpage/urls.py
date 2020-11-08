103e70b9d1b1ad3a1615270053e80aff83975807
Ashish Rout <ashishrout1999@gmail.com>
2020-11-09 01:52:01 +0530

    Added Sign up and Login functionality

--- aNANt/frontpage/urls.py
+++ aNANt/frontpage/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('contact/',views.contact,name='contact'),
     path('publications/',views.publications,name='publications'),
     path('acknowledgements/',views.acknowledgements,name='acknowledgements'),
+    path('login_page/', views.login_page, name = 'login_page'),
+    path('login_page/login/', views.login_view, name = 'login'),
+    path('logout/', views.logout_view, name = 'logout'),
+    path('login_page/register/', views.register, name = 'register'),
 ]
