da0195094eb614ae087a1c66beb4cfe3dff4f08b
Prajjwal Das <55483277+prajjwal27@users.noreply.github.com>
2021-03-10 01:03:33 +0530

    Update models.py

--- aNANt/superclass/models.py
+++ aNANt/superclass/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Materials(models.Model) :
+
+    # name of the mxene
+
+    name = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
